cd966d92a66bebb4b4714231dd441232bb65922a
Jiyad <mdjiyade@gmail.com>
2026-02-25 20:16:26 +0530

    Enhance UI by adding title to login, signup, and forgot password pages

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -136,11 +136,14 @@ const ForgotPassword = () => {
         <>
             <div className='flex flex-col justify-center gap-10 items-center min-h-dvh w-full relative bg-bg-light dark:bg-bg-dark py-3'>
                 <title>Forgot Password</title>
-                <div className=''>
+                <div className='flex flex-col items-center gap-3 px-3'>
                     <Logo
                         width={150}
                         height={150}
                     />
+                    <h1 className='text-accent text-base sm:text-xl font-bold text-center'>
+                        Digital College Election Management System
+                    </h1>
                 </div>
                 <div className='w-11/12 md:w-9/12 lg:w-7/12 xl:w-5/12 2xl:w-4/12 bg-card-light dark:bg-card-dark rounded-xl shadow-lg flex flex-col items-center gap-10 px-10 py-11'>
                     <Title
